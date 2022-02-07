d7c4ac269b81134b15609f15da71a624dc6d07a2
Luka Huang <szteven3@gmail.com>
2022-02-07 09:50:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     user_id = event.source.user_id
-    print(user_id)
+    print('user_id:' + user_id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="luka: " + event.message.text)
